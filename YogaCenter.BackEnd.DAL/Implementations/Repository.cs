59eef003fe54f4745ea9e5eb0317a742207a24d9
HONG QUAN <hongquan.contact@gmail.com>
2023-10-07 08:58:50 +0700

    unit of work

---
+++ YogaCenter.BackEnd.DAL/Implementations/Repository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using YogaCenter.BackEnd.DAL.Contracts;
+using YogaCenter.BackEnd.DAL.Data;
+
+namespace YogaCenter.BackEnd.DAL.Implementations
+{
+    public class Repository<T> :IRepository<T>  where T : class
+    {
+        private readonly YogaCenterContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(YogaCenterContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet.AsNoTracking();
+        }
+
+        public T GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(object id)
+        {
+            T entityToDelete = _dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public void Delete(T entity)
+        {
+            if (_context.Entry(entity).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entity);
+            }
+            _dbSet.Remove(entity);
+        }
+    }
+}
